28077111dd1b799bd2483d3c1fbb6e5e02648e63
agabrielagomes <gabriela.lgs@Outlook.com>
2025-05-08 01:29:12 -0300

    Criação das telas de cadastro das empresa e link nos botões

---
+++ script/form01emp.js
@@ -0,0 +1,4 @@
+document.getElementById("cadastroForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  window.location.href = "form02emp.html"; // Caminho correto dentro da pasta /index
+});
